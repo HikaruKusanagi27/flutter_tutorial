845f623e0673946c6f6fd14b53aef62921e0fcaa
HikaruKusanagi <hikarugp24@gmail.com>
2025-03-30 21:04:09 +0900

    no message

--- lib/googlemap/googlemap_page.dart
+++ lib/googlemap/googlemap_page.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+class GoogleMapPage extends StatelessWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Google Map')),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.7749, -122.4194),
+          zoom: 12,
+        ),
+      ),
+    );
+  }
+}
